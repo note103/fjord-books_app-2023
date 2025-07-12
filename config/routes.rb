dce3a2205a1c313b006bc2b5cac97bad4f67dfa0
Hiroaki Kadomatsu <note103@gmail.com>
2025-07-13 00:40:41 +0900

    letter_opener_webを追加し、開発環境でのメール送信設定を更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   resources :users, only: [:index, :show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  # Defines the root path route ("/")
   root "books#index"
 end
