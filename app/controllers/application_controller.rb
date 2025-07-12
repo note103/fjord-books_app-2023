436f3a09c8a8e683f66b8f825bfb666d559413e3
Hiroaki Kadomatsu <note103@gmail.com>
2025-07-13 02:45:54 +0900

    ユーザー登録およびアカウント更新時にカスタムフィールドの登録・更新を許可するパラメータを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    custom_keys = %i[postal_code address bio]
+    devise_parameter_sanitizer.permit(:sign_up, keys: custom_keys)
+    devise_parameter_sanitizer.permit(:account_update, keys: custom_keys)
+  end
 end
